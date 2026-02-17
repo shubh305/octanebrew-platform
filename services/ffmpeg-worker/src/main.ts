414136de5413e0eeb3fc26b21269b23f903cb32a
shubh305 <shubham.305singh@gmail.com>
2026-02-17 15:36:38 +0000

    feat(ops): Added kafka broker SASL auth in ffmpeg worker

--- services/ffmpeg-worker/src/main.ts
+++ services/ffmpeg-worker/src/main.ts
@@ -3,15 +3,24 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const isSaslEnabled = !!process.env.KAFKA_BROKER_USER;
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.KAFKA,
       options: {
         client: {
-          brokers: [process.env.KAFKA_BROKERS || 'broker.octanebrew.dev:8084'],
+          brokers: [process.env.KAFKA_BOOTSTRAP_SERVERS || ''],
           connectionTimeout: 10000,
           requestTimeout: 30000,
+          sasl: isSaslEnabled
+            ? {
+                mechanism: 'plain',
+                username: process.env.KAFKA_BROKER_USER!,
+                password: process.env.KAFKA_BROKER_PASS!,
+              }
+            : undefined,
         },
         consumer: {
           groupId: 'ffmpeg-worker-consumer',
